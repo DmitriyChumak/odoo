4f002011bee382c6dc73c8f1f4dc8bb7204a49b1
DmitriyChumak <dimitriy.chumak@gmail.com>
2024-07-18 22:32:54 +0300

    4.6 is full done

---
+++ school_lesson_6_1/models/library_book.py
@@ -0,0 +1,16 @@
+from odoo import models, fields
+
+class LibraryBook(models.Model):
+    _name = 'library.book'
+    _description = 'Library Book'
+
+    name = fields.Char(string='Name', required=True)
+    category_id = fields.Many2one(
+        comodel_name='library.book.category',
+        string='Category'
+    )
+    user_id = fields.Many2one(
+        comodel_name='res.users',
+        string='User',
+        default=lambda self: self.env.ref('base.user_admin')
+    )
